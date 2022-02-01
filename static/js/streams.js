eb56a333eab897d197354c3786cd6e66b988d99a
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-02-01 14:47:50 +0600

    video audio fixed

--- static/js/streams.js
+++ static/js/streams.js
@@ -7,9 +7,11 @@ const client = AgoraRTC.createClient({mode: 'rtc', codec: 'vp8'})
 let localTracks = []
 let remoteUsers = {}
 let joinAndDisplayLocalStream = async () => {
-    client.on('user-published', () => {
-        console.log('user has join our room!')
-    })
+    client.on('user-published', handleUserJoined)
+    client.on('user-left', handleUserLeft)
+    // client.on('user-published', () => {
+    //     console.log('user has join our room!')
+    // })
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
 
@@ -52,5 +54,42 @@ let handleUserJoined = async (user, mediaType) => {
     }
 }
 
-joinAndDisplayLocalStream()
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
+}
 
+let leaveAndRemoveLocalStream = async () => {
+    for (let i = 0; localTracks.length > i; i++) {
+        localTracks[i].stop()
+        localTracks[i].close()
+    }
+    await client.leave()
+    window.open('/', '_self')
+}
+
+let toggleCamera = async (e) => {
+    if (localTracks[1].muted) {
+        await localTracks[1].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    } else {
+        await localTracks[1].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+}
+
+let toggleMic = async (e) => {
+    if (localTracks[0].muted) {
+        await localTracks[0].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    } else {
+        await localTracks[0].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+}
+
+joinAndDisplayLocalStream()
+document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
+
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
